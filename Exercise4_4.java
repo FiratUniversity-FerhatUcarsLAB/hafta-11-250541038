f4f9e4f82d9c491e722ae344beb94e480bd06b7c
250541038 <250541038@firat.edu.tr>
2025-12-03 21:43:47 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -11,11 +11,21 @@ public class Exercise4_4 {
     public static void main(String[] args) {
 
         // 1) Donus degerini kullanmayin -> ne oluyor?
+        
         getNumber();
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
 
         // Cevaplarinizi yorum olarak ekleyin.
+        //1. Soru:
+        //Java'da bu hata vermez. Metot çalışır,
+        //işini yapar, dönen değer hafızada bir yere atanmadığı için kaybolur.
+
+        //2. Soru:
+        //Java derleme hatası (compile-time error) verir.
+        //Çünkü void metotlar bir değer üretmez,
+        //dolayısıyla matematiksel veya mantıksal bir işleme sokulamazlar.
+    
     }
 }
