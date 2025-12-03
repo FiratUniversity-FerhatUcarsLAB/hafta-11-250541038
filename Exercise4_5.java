7990c30e320851da70291d52cfe3b5add0bd97d1
250541038 <250541038@firat.edu.tr>
2025-12-03 21:48:21 +0300

    Update Exercise4_5.java

--- Exercise4_5.java
+++ Exercise4_5.java
@@ -21,7 +21,28 @@ public class Exercise4_5 {
         zoop("breakfast ", fork);
     }
 
+
     public static void ping(String strangStrung) {
         System.out.println("any " + strangStrung + "more ");
     }
 }
+
+//main zoop "just for", 5 çağırır çağırı biter .
+//main: clink(2*2) yani clink(4) çağırır.
+//clink: "It's " yazar ve zoop("breakfast", 4) çağırır (2. çağrı).
+//main:
+//args: (boş dizi)
+
+//bizz: 5
+//buzz: 2
+//clink:
+//fork: 4
+//zoop:
+//fred: "breakfast"
+//bob: 4
+
+     //Çıktı:
+    //just for
+    //any not more 
+    //It's breakfast
+    //!
