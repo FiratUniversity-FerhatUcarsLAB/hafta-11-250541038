2332fbe5e0065f03f18bc6b076cff8e3ac543a3b
250541038 <250541038@firat.edu.tr>
2025-12-03 21:49:35 +0300

    Update Exercise4_6_Multadd.java

--- Exercise4_6_Multadd.java
+++ Exercise4_6_Multadd.java
@@ -1,16 +1,31 @@
-public class Exercise4_6_Multadd {
-
-    public static double multadd(double a, double b, double c) {
-        // TODO
-        return 0.0;
-    }
-
-    public static double expSum(double x) {
-        // TODO
-        return 0.0;
-    }
+public class Multadd {
 
     public static void main(String[] args) {
-        // TODO: test methods
+        
+        System.out.println("Multadd Testi (1, 2, 3): " + multadd(1.0, 2.0, 3.0));
+
+        
+        double sinVal = Math.sin(Math.PI / 4);
+        double cosVal = Math.cos(Math.PI / 4) / 2;
+        double result1 = multadd(1.0, sinVal, cosVal); 
+        System.out.println("Sin/Cos İşlemi Sonucu: " + result1);
+
+        System.out.println("Log İşlemi Sonucu: " + logResult);
+
+        double x = 2.0;
+        System.out.println("expSum(" + x + ") Sonucu: " + expSum(x));
+    }
+
+    public static double multadd(double a, double b, double c) {
+        return a * b + c;
+    }
+
+    
+    public static double expSum(double x) {
+        double eNegX = Math.exp(-x);
+        double sqrtPart = Math.sqrt(1.0 - eNegX);
+        
+    
+        return multadd(x, eNegX, sqrtPart);
     }
 }
